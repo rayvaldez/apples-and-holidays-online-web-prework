884a6d75b358aa635c01512feedd299544325862
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-20 09:52:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,23 +1,6 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
-  # given that holiday_hash looks like this:
-  # {
-  #   :winter => {
-  #     :christmas => ["Lights", "Wreath"],
-  #     :new_years => ["Party Hats"]
-  #   },
-  #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
-  #   },
-  #   :fall => {
-  #     :thanksgiving => ["Turkey"]
-  #   },
-  #   :spring => {
-  #     :memorial_day => ["BBQ"]
-  #   }
-  # }
-  # return the second element in the 4th of July array
 
   holiday_hash[:summer][:fourth_of_july][1]
 end
@@ -34,25 +17,19 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
+
   holiday_hash[season][holiday_name] = supply_array
-      # remember to return the updated hash
+
   holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
+
   holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
+
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
       holiday.each do |holiday, supplies|
@@ -64,15 +41,12 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
   array = []
      holiday_hash.each do |season, holiday_name|
       holiday_name.each do |holiday, supplies|
         supplies.each do |supply|
           if supply == "BBQ"
             array << holiday
-            binding.pry
           end
         end
       end
